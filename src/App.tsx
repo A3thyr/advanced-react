ac392dfc2a805fc128375251f1198d6dc820e01d
A3thyr <a3therys@gmail.com>
2025-09-25 00:25:28 +0300

    init: added routes + some suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Counter } from "./components/counter";
+import { Link, Route, Routes } from "react-router-dom";
 import "./index.scss";
+import { AboutPageLazy } from "./pages/AboutPage/about.lazy";
+import { MainPageLazy } from "./pages/MainPage/main.lazy";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="">
-      <Counter />
+      <Link to={`/`} style={{ backgroundColor: "violet" }}>
+        Main Page
+      </Link>
+      <Link to={`/about`} style={{ backgroundColor: "red" }}>
+        About Page
+      </Link>
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path="/" element={<MainPageLazy />} />
+          <Route path={`/about`} element={<AboutPageLazy />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
