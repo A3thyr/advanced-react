3c9a148738a1b00b15f49d17b74bffc2b9e21375
A3thyr <a3therys@gmail.com>
2025-10-07 20:49:11 +0300

    fix: fixed test render

--- src/shared/config/i18n/i18nForTests.ts
+++ src/shared/config/i18n/i18nForTests.ts
@@ -1,29 +1,21 @@
 import i18n from "i18next";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    //   lng: "en",
-    fallbackLng: "en",
+i18n.use(initReactI18next).init({
+  lng: "en",
+  fallbackLng: "en",
 
-    debug: true,
+  debug: false,
 
-    interpolation: {
-      escapeValue: false, // not needed for react!!
-    },
+  // interpolation: {
+  //   escapeValue: false, // not needed for react!!
+  // },
 
-    ns: ["translation", "main", "about"],
-    defaultNS: "translation",
-    load: "currentOnly",
-
-    //   resources: { en: { translations: {} } },
-
-    backend: {
-      loadPath: "/locales/{{lng}}/{{ns}}.json",
-    },
-  });
+  ns: ["translation", "main", "about"],
+  defaultNS: "translation",
+  // load: "currentOnly",
+  initImmediate: false,
+  resources: { en: { translations: {} } },
+});
 
 export default i18n;
---
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -0,0 +1,26 @@
+import { fireEvent, screen, waitFor } from "@testing-library/react";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
+import { Sidebar } from "./Sidebar";
+
+describe("Sidebar", () => {
+  test("should render Sidebar component", async () => {
+    componentRender(<Sidebar />);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+    });
+  });
+  test("Sidebar should toggle its width", async () => {
+    componentRender(<Sidebar />);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+    });
+
+    const toggleBtn = screen.getByTestId("sidebar-toggle");
+
+    fireEvent.click(toggleBtn);
+    // screen.debug();
+    expect(screen.getByTestId("sidebar")).toHaveClass("collapsed");
+  });
+});
