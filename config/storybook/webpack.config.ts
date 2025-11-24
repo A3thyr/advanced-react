3a283ad4b169560aa4e44afdd92c06fa21825b0a
A3thyr <a3therys@gmail.com>
2025-11-24 19:44:15 +0300

    fix: fixed storybook config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,10 +13,10 @@ export default ({ config }: { config: WebpackConfiguration }) => {
     src: path.resolve(__dirname, "..", "..", "src"),
   };
 
-  config.resolve.modules.push(paths.src);
-  config.resolve.extensions.push(".ts", ".tsx");
+  config!.resolve!.modules!.push(paths.src);
+  config!.resolve!.extensions!.push(".ts", ".tsx");
 
-  config.plugins.push(
+  config!.plugins!.push(
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(true),
       __API__: JSON.stringify(""),
@@ -24,15 +24,16 @@ export default ({ config }: { config: WebpackConfiguration }) => {
   );
 
   // eslint-disable-next-line no-param-reassign
-  config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+  // @ts-ignore
+  config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
     }
     return rule;
   });
 
-  config.module.rules.push(buildSvgLoader());
-  config.module.rules.push(buildCssLoaders(true));
+  config!.module!.rules.push(buildSvgLoader());
+  config!.module!.rules.push(buildCssLoaders(true));
 
   return config;
 };
