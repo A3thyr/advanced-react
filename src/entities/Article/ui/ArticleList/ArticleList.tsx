776e077eb3f0cee86f52bb0aca1e9d755f7b5a4d
A3thyr <a3therys@gmail.com>
2026-02-28 18:22:16 +0300

    feat: added baseline for article list

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,9 +1,29 @@
+import { Article, ArticleView } from "entities/Article/model/types/article";
 import { FC } from "react";
+import { classNames } from "shared/lib/helpers/classNames/classNames.helper";
+import { ArticleListItem } from "../ArticleListItem/ArticleListItem";
+import cls from "./ArticleList.module.scss";
 
 interface ArticleListProps {
   className?: string;
+  articles: Article[];
+  isLoading?: boolean;
+  view?: ArticleView;
 }
 
-export const ArticleList: FC<ArticleListProps> = () => {
-  return null;
+export const ArticleList: FC<ArticleListProps> = ({
+  className,
+  articles,
+  isLoading,
+  view = ArticleView.SMALL,
+}) => {
+  const renderArticle = (article: Article) => {
+    return <ArticleListItem key={article.id} article={article} view={view} />;
+  };
+
+  return (
+    <div className={classNames(cls.ArticleList, {}, [className])}>
+      {articles.map(renderArticle)}
+    </div>
+  );
 };
--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,9 +1,22 @@
+import { Article, ArticleView } from "entities/Article/model/types/article";
 import { FC } from "react";
+import { classNames } from "shared/lib/helpers/classNames/classNames.helper";
+import cls from "./ArticleListItem.module.scss";
 
 interface ArticleListItemProps {
   className?: string;
+  article: Article;
+  view?: ArticleView;
 }
 
-export const ArticleListItem: FC<ArticleListItemProps> = () => {
-  return null;
+export const ArticleListItem: FC<ArticleListItemProps> = ({
+  className,
+  article,
+  view = ArticleView.SMALL,
+}) => {
+  return (
+    <div className={classNames(cls.ArticleListItem, {}, [className])}>
+      {article.title}
+    </div>
+  );
 };
